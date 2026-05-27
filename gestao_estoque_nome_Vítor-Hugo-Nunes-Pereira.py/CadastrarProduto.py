efd24877e26bb47c868ca08015dc04b608605feb
Isaac Araujo Torres Resende <resendeisaac@gmail.com>
2026-05-26 21:23:07 -0300

    código melhorado e comentado
    Melhoria na organização do código e adição de comentários explicativos.

--- gestao_estoque_nome_Vítor-Hugo-Nunes-Pereira.py/CadastrarProduto.py
+++ gestao_estoque_nome_Vítor-Hugo-Nunes-Pereira.py/CadastrarProduto.py
@@ -1,15 +1,21 @@
+# Dicionário que armazena todos os produtos cadastrados
 produtos = {}
 
+# Variável responsável por gerar IDs únicos para os produtos
 proximo_id = 1
 
+# Função responsável por cadastrar novos produtos no estoque
 def cadastrar_produto(nome, categoria, preco, quantidade, estoque=None):
     """Registra novo produto no sistema"""
 
+    # Permite alterar a variável global proximo_id
     global proximo_id
 
+    # Caso nenhum estoque seja informado, usa o dicionário principal
     if estoque is None:
         estoque = produtos
 
+     # Adiciona o produto ao dicionário usando o ID atual
     estoque[proximo_id] = {
         "nome": nome,
         "categoria": categoria,
@@ -17,7 +23,7 @@ def cadastrar_produto(nome, categoria, preco, quantidade, estoque=None):
         "quantidade": quantidade
     }
 
-    print(f"\nProduto cadastrado com sucesso!")
-    print(f"ID do produto: {proximo_id}")
+    print(f"\nProduto cadastrado com sucesso!")   # Exibe mensagem de sucesso
+    print(f"ID do produto: {proximo_id}")   # Mostra o ID gerado para o produto
 
-    proximo_id += 1
+    proximo_id += 1       # Incrementa o ID para o próximo cadastro
